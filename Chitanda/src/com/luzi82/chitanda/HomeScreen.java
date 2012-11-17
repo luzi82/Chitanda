2b3a6e9e225c0b1ada780cf12288aeb2e40135a9
luzi82 <luzi82@gmail.com>
2012-11-17 16:46:21 +0800

    create screens

---
+++ Chitanda/src/com/luzi82/chitanda/HomeScreen.java
@@ -0,0 +1,11 @@
+package com.luzi82.chitanda;
+
+import com.luzi82.gdx.GrScreen;
+
+public class HomeScreen extends GrScreen<ChitandaGame> {
+
+	public HomeScreen(ChitandaGame aChitandaGame) {
+		super(aChitandaGame);
+	}
+
+}
---
+++ Chitanda/src/com/luzi82/chitanda/game/ui/GameScreen.java
@@ -0,0 +1,12 @@
+package com.luzi82.chitanda.game.ui;
+
+import com.luzi82.chitanda.ChitandaGame;
+import com.luzi82.gdx.GrScreen;
+
+public class GameScreen extends GrScreen<ChitandaGame> {
+
+	public GameScreen(ChitandaGame aParent) {
+		super(aParent);
+	}
+
+}
