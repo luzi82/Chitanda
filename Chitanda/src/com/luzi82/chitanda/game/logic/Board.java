48f9bfb0633c33a3e56d68b3681ee654ec9b5fa0
luzi82 <luzi82@gmail.com>
2012-11-17 16:46:30 +0800

    create board

---
+++ Chitanda/src/com/luzi82/chitanda/game/logic/Board.java
@@ -0,0 +1,10 @@
+package com.luzi82.chitanda.game.logic;
+
+public class Board {
+
+	public static int WIDTH = 1920 * 4;
+	public static int HEIGHT = 1080 * 4;
+
+	boolean[][] cell = new boolean[WIDTH][HEIGHT];
+
+}
