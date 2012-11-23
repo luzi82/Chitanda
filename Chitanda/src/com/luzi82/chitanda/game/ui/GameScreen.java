bf2b128ffa9daa784c0274e4769058309c7c3b21
luzi82 <luzi82@gmail.com>
2012-11-23 22:19:32 +0800

    add border line when zoom in

--- Chitanda/src/com/luzi82/chitanda/game/ui/GameScreen.java
+++ Chitanda/src/com/luzi82/chitanda/game/ui/GameScreen.java
@@ -2,6 +2,7 @@ package com.luzi82.chitanda.game.ui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GLCommon;
 import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
@@ -20,18 +21,28 @@ public class GameScreen extends GrScreen<ChitandaGame> {
 	public static final float DIV_LN_SMOOTH_REDUCE = (float) (1 / Math.log(SMOOTH_REDUCE));
 	public static final float PHI = (float) (1 + Math.sqrt(5)) / 2;
 
+	private static final float LINE_ALPHA = 1f / 16;
+	private static final float LINE_WIDTH = 1f / 16;
+
 	private Board mBoard;
 
 	private OrthographicCamera mCamera;
-	// private SpriteBatch mBatch;
-	private Pixmap mPixmap0;
-	private Pixmap mPixmap1;
-	private Texture mTexture0;
-	private Texture mTexture1;
-	// private Sprite mSprite0;
-	// private Sprite mSprite1;
-	private Mesh mMesh0;
-	private Mesh mMesh1;
+
+	private Pixmap mBasePixmap0;
+	private Pixmap mBasePixmap1;
+	private Texture mBaseTexture0;
+	private Texture mBaseTexture1;
+	private Mesh mBaseMesh0;
+	private Mesh mBaseMesh1;
+
+	private Mesh mLineMeshH;
+	private float[] mLineMeshHF;
+	private final int[] LINE_MESH_HA = {//
+	1 * 7 - 1,//
+			2 * 7 - 1,//
+			7 * 7 - 1,//
+			8 * 7 - 1,//
+	};
 
 	private float mViewPortWidth;
 	private float mViewPortHeight;
@@ -79,35 +90,58 @@ public class GameScreen extends GrScreen<ChitandaGame> {
 
 		mCamera = new OrthographicCamera();
 
-		mPixmap0 = new Pixmap(Gdx.files.internal("data/chitanda0.png"));
-		mPixmap1 = new Pixmap(Gdx.files.internal("data/chitanda1.png"));
-		mTexture0 = new Texture(mPixmap0, true);
-		mTexture1 = new Texture(mPixmap1, true);
+		mBasePixmap0 = new Pixmap(Gdx.files.internal("data/chitanda0.png"));
+		mBasePixmap1 = new Pixmap(Gdx.files.internal("data/chitanda1.png"));
+		mBaseTexture0 = new Texture(mBasePixmap0, true);
+		mBaseTexture1 = new Texture(mBasePixmap1, true);
 
 		VertexAttributes va = new VertexAttributes( //
 				new VertexAttribute(VertexAttributes.Usage.Position, 3, "position"),//
 				new VertexAttribute(VertexAttributes.Usage.TextureCoordinates, 2, "texturecoordinates")//
 		);
 
-		mMesh0 = new Mesh(true, 4, 4, va);
-		mMesh0.setVertices(new float[] { //
-		0f, 1024f * 4, 0f, 0f, 0f,//
-				1024f * 4, 1024f * 4, 0f, 1f, 0f,//
-				0f, 0f, 0f, 0f, 1f,//
-				1024f * 4, 0f, 0f, 1f, 1f,//
-		});
-		mMesh0.setIndices(new short[] { 0, 1, 2, 3 });
-		mMesh1 = new Mesh(true, 4, 4, va);
-		mMesh1.setVertices(new float[] { //
-		1024f * 4, 1024f * 4, 0f, 0f, 0f,//
-				1024f * 8, 1024f * 4, 0f, 1f, 0f,//
-				1024f * 4, 0f, 0f, 0f, 1f,//
-				1024f * 8, 0f, 0f, 1f, 1f,//
-		});
-		mMesh1.setIndices(new short[] { 0, 1, 2, 3 });
+		mBaseMesh0 = new Mesh(true, 4, 4, va);
+		mBaseMesh0.setVertices(new float[] { //
+				0f, 1024f * 4, 0f, 0f, 0f,//
+						1024f * 4, 1024f * 4, 0f, 1f, 0f,//
+						0f, 0f, 0f, 0f, 1f,//
+						1024f * 4, 0f, 0f, 1f, 1f,//
+				});
+		mBaseMesh0.setIndices(new short[] { 0, 1, 2, 3 });
+		mBaseMesh1 = new Mesh(true, 4, 4, va);
+		mBaseMesh1.setVertices(new float[] { //
+				1024f * 4, 1024f * 4, 0f, 0f, 0f,//
+						1024f * 8, 1024f * 4, 0f, 1f, 0f,//
+						1024f * 4, 0f, 0f, 0f, 1f,//
+						1024f * 8, 0f, 0f, 1f, 1f,//
+				});
+		mBaseMesh1.setIndices(new short[] { 0, 1, 2, 3 });
+
+		va = new VertexAttributes( //
+				new VertexAttribute(VertexAttributes.Usage.Position, 3, "position"),//
+				new VertexAttribute(VertexAttributes.Usage.Color, 4, "color")//
+		);
+
+		mLineMeshH = new Mesh(false, 8, 12, va);
+		mLineMeshH.setIndices(new short[] { //
+				0, 1, 2, //
+						1, 2, 3, //
+						4, 5, 6, //
+						5, 6, 7 //
+				});
+		mLineMeshHF = new float[] { //
+		1f, 0f, 0f, 0f, 0f, 0f, LINE_ALPHA,//
+				0f, 0f, 0f, 0f, 0f, 0f, LINE_ALPHA,//
+				1f, LINE_WIDTH, 0f, 0f, 0f, 0f, 0f,//
+				0f, LINE_WIDTH, 0f, 0f, 0f, 0f, 0f,//
+
+				1f, 1 - LINE_WIDTH, 0f, 0f, 0f, 0f, 0f,//
+				0f, 1 - LINE_WIDTH, 0f, 0f, 0f, 0f, 0f,//
+				1f, 1f, 0f, 0f, 0f, 0f, LINE_ALPHA,//
+				0f, 1f, 0f, 0f, 0f, 0f, LINE_ALPHA,//
+		};
 
 		gl.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-		gl.glEnable(GL10.GL_TEXTURE_2D);
 
 		// mTouching = new boolean[TOUCH_MAX];
 		// mLastX = new int[TOUCH_MAX];
@@ -217,10 +251,69 @@ public class GameScreen extends GrScreen<ChitandaGame> {
 		}
 		mCamera.apply(gl);
 
-		mTexture0.bind();
-		mMesh0.render(GL10.GL_TRIANGLE_STRIP, 0, 4);
-		mTexture1.bind();
-		mMesh1.render(GL10.GL_TRIANGLE_STRIP, 0, 4);
+		// back image
+		gl.glDisable(GL10.GL_BLEND);
+		gl.glBlendFunc(GL10.GL_ONE, GL10.GL_ZERO);
+		gl.glEnable(GL10.GL_TEXTURE_2D);
+		mBaseTexture0.bind();
+		mBaseMesh0.render(GL10.GL_TRIANGLE_STRIP);
+		mBaseTexture1.bind();
+		mBaseMesh1.render(GL10.GL_TRIANGLE_STRIP);
+
+		// line
+		gl.glEnable(GL10.GL_BLEND);
+		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		gl.glDisable(GL10.GL_TEXTURE_2D);
+
+		if (iCameraZoom < ZOOM_MIN * PHI * PHI) {
+			float a = iCameraZoom / (ZOOM_MIN * PHI);
+			a = (float) Math.log(a);
+			a /= (float) Math.log(PHI);
+			a = 1 - a;
+			if (a > 0) {
+				if (a > 1)
+					a = 1f;
+				a *= LINE_ALPHA;
+				for (int ai : LINE_MESH_HA) {
+					mLineMeshHF[ai] = a;
+				}
+				mLineMeshH.setVertices(mLineMeshHF);
+
+				int min, max;
+
+				min = (int) Math.floor(iCameraY - iCameraZoom * mViewPortHeight / 2);
+				max = (int) Math.ceil(iCameraY + iCameraZoom * mViewPortHeight / 2);
+				if (min < 0)
+					min = 0;
+				if (max > Board.HEIGHT)
+					max = Board.HEIGHT;
+				for (i = min; i < max; ++i) {
+					gl.glPushMatrix();
+					gl.glTranslatef(0, i, 0);
+					gl.glScalef(Board.WIDTH, 1, 1);
+					mLineMeshH.render(GL10.GL_TRIANGLES);
+					gl.glPopMatrix();
+				}
+
+				min = (int) Math.floor(iCameraX - iCameraZoom * mViewPortWidth / 2);
+				max = (int) Math.ceil(iCameraX + iCameraZoom * mViewPortWidth / 2);
+				if (min < 0)
+					min = 0;
+				if (max > Board.WIDTH)
+					max = Board.WIDTH;
+				gl.glPushMatrix();
+				gl.glRotatef(90, 0, 0, 1);
+				gl.glScalef(1, -1, 1);
+				for (i = min; i < max; ++i) {
+					gl.glPushMatrix();
+					gl.glTranslatef(0, i, 0);
+					gl.glScalef(Board.HEIGHT, 1, 1);
+					mLineMeshH.render(GL10.GL_TRIANGLES);
+					gl.glPopMatrix();
+				}
+				gl.glPopMatrix();
+			}
+		}
 	}
 
 	@Override
