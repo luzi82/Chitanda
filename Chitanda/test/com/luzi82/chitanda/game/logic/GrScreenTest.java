db78ba68ac1f72987afd123160ac52d0349fd60d
luzi82 <luzi82@gmail.com>
2012-11-25 21:45:17 +0800

    dispose array object

---
+++ Chitanda/test/com/luzi82/chitanda/game/logic/GrScreenTest.java
@@ -0,0 +1,93 @@
+package com.luzi82.chitanda.game.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.badlogic.gdx.utils.Disposable;
+import com.luzi82.gdx.GrGame;
+import com.luzi82.gdx.GrScreen;
+
+public class GrScreenTest {
+
+	static int mStaticV;;
+
+	@Test
+	public void breadthDispose() {
+
+		class D implements Disposable {
+			@Override
+			public void dispose() {
+				++mStaticV;
+			}
+		}
+
+		class T extends GrScreen<GrGame> {
+			@SuppressWarnings("unused")
+			private D mD = new D();
+
+			protected T(GrGame aParent) {
+				super(aParent);
+			}
+		}
+
+		T t = new T(null);
+		mStaticV = 0;
+		t.dispose();
+		assertEquals(1, mStaticV);
+
+	}
+
+	@Test
+	public void arrayDispose() {
+
+		class D implements Disposable {
+			@Override
+			public void dispose() {
+				++mStaticV;
+			}
+		}
+
+		class T extends GrScreen<GrGame> {
+			private D[] mD = new D[1];
+
+			protected T(GrGame aParent) {
+				super(aParent);
+				mD[0] = new D();
+			}
+		}
+
+		T t = new T(null);
+		mStaticV = 0;
+		t.dispose();
+		assertEquals(1, mStaticV);
+
+	}
+
+	@Test
+	public void array2Dispose() {
+
+		class D implements Disposable {
+			@Override
+			public void dispose() {
+				++mStaticV;
+			}
+		}
+
+		class T extends GrScreen<GrGame> {
+			private D[][] mD = new D[1][1];
+
+			protected T(GrGame aParent) {
+				super(aParent);
+				mD[0][0] = new D();
+			}
+		}
+
+		T t = new T(null);
+		mStaticV = 0;
+		t.dispose();
+		assertEquals(1, mStaticV);
+
+	}
+
+}
